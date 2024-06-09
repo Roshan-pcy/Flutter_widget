0dc4107fbc59be4154051c67373a79a881f5c6b6
Roshan pcy <godxrosu@gmail.com>
2024-06-09 13:01:49 +0530

    added

--- all_widgets/lib/3dContianer.dart
+++ all_widgets/lib/3dContianer.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
-class Container3d extends StatelessWidget {
+class Container3d extends StatefulWidget {
   const Container3d({super.key});
 
   @override
+  State<Container3d> createState() => _Container3dState();
+}
+
+class _Container3dState extends State<Container3d> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+          child: Container(
+        height: 100,
+        width: 100,
+        decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.circular(25),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.shade500,
+                  blurRadius: 15,
+                  spreadRadius: 1.0,
+                  offset: Offset(4, 4)),
+              BoxShadow(
+                  color: Colors.white,
+                  blurRadius: 15,
+                  spreadRadius: 1.0,
+                  offset: Offset(-4, -4))
+            ]),
+        child: const Icon(
+          Icons.android,
+          size: 50,
+        ),
+      )),
+    );
   }
 }
--- all_widgets/lib/main.dart
+++ all_widgets/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:all_widgets/3dContianer.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,21 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Container3d());
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
- 
 
   final String title;
 
@@ -38,25 +35,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-     
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-     
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-     
         title: Text(widget.title),
       ),
       body: Center(
-     
         child: Column(
-       
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -73,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ),  
+      ),
     );
   }
 }
